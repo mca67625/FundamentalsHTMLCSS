df4dbe025ca599e7b1c349535c8605a66baa8d05
mca67625 <mca67625@gmail.com>
2024-06-05 16:31:25 -0400

    second commit

--- src/main/java/com/mark/fundamentalshtmlcss/controller/FundamentalsController.java
+++ src/main/java/com/mark/fundamentalshtmlcss/controller/FundamentalsController.java
@@ -1,2 +1,28 @@
-package com.mark.fundamentalshtmlcss.controller;public class FundamentalsController {
+package com.mark.fundamentalshtmlcss.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller //handles incoming web request
+public class FundamentalsController {
+
+    @GetMapping("/") //endpoint localhost:8080/
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/fundamentals") //endpoint localhost:8080/fundamentals
+    public String fundamentals() {
+        return "fundamentals";
+    }
+
+    @GetMapping("/elements") //endpoint localhost:8080/elements
+    public String elements() {
+        return "elements";
+    }
+
+    @GetMapping("/resume") //endpoint localhost:8080/
+    public String resume() {
+        return "resume";
+    }
 }
